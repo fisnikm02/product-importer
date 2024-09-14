490adf5266d707080218062793f0455b41adc924
Fisnik Murati <fisnikm02@Fisniks-MacBook-Air.local>
2024-09-13 21:15:16 -0700

    finished

--- src/gpt.service.ts
+++ src/gpt.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import { ChatOpenAI } from '@langchain/openai';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Injectable()
 export class GPTService {
+    private openai: ChatOpenAI;
+
+    constructor() {
+        this.openai = new ChatOpenAI({
+            apiKey: process.env.APP_OPENAI_KEY,
+            modelName: 'gpt-4',
+            temperature: 0.7,
+        });
+    }
+
     async enhanceDescription(name: string, description: string, category?: string): Promise<string> {
         const prompt = `
             You are an expert in medical sales. Your specialty is medical consumables used by hospitals daily. Your task is to enhance the description of a product based on the information provided.
@@ -20,22 +30,11 @@ export class GPTService {
         return gptResponse;
     }
 
-    async callGPT4(prompt: string): Promise<string> {
+    async callGPT4(prompt: string): Promise<any> {
         try {
-            const apiKey = process.env.APP_OPENAI_KEY_OMER;
-            const response = await axios.post('https://api.openai.com/v1/completions', {
-                model: 'gpt-4',
-                prompt: prompt,
-                max_tokens: 200,
-                temperature: 0.7
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${apiKey}`,
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            return response.data.choices[0].text.trim();
+            const response = await this.openai.invoke(prompt);
+            console.log(response)
+            return response;
         } catch (error) {
             console.error('Error calling GPT-4 API:', error);
             throw new Error('Failed to enhance the description.');
